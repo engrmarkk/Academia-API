7f6e00b931a058bb77dda669ade5731d1b86134e
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-05 10:15:02 +0100

    student table name

--- api/models/students.py
+++ api/models/students.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 
 class Student(db.Model):
+    __tablename__ = 'students'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
