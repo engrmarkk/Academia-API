bdbf9a2fd2f26ec7c8dcf254ba5acc687112fd90
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-05 12:28:39 +0100

    relationship between student and course registered

--- api/models/students.py
+++ api/models/students.py
@@ -1,5 +1,4 @@
 from ..extensions import db
-from enum import Enum
 
 
 class Student(db.Model):
@@ -10,6 +9,7 @@ class Student(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    registered_courses = db.relationship('CourseRegistered', cascade="all, delete", backref='student', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
