d068594367dea19210cd9b2ac5c1b2229c72e172
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-05 10:13:50 +0100

    courses model

---
+++ api/models/courses.py
@@ -0,0 +1,18 @@
+from ..extensions import db
+from datetime import datetime
+
+
+class Course(db.Model):
+    __tablename__ = 'courses'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    code = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    semester = db.Column(db.Integer, nullable=False)
+    year = db.Column(db.Integer, nullable=False, default=datetime.now().year)
+    unit = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    tutor_id = db.Column(db.Integer, db.ForeignKey('staffs.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Course %r>' % self.name
