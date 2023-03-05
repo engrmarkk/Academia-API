464ca9560cd3cbca3d8b146d6e9cccc24a55a446
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-05 13:49:00 +0100

    jwt and flask_smorest extensions

--- api/extensions/__init__.py
+++ api/extensions/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_smorest import Api
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 migrate = Migrate()
+jwt = JWTManager()
+api = Api()
