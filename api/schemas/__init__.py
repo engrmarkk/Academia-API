dca13ff526065a23d16b0c6b809ef0e797a6238b
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-08 17:11:58 +0100

    schemas

--- api/schemas/__init__.py
+++ api/schemas/__init__.py
@@ -1,14 +1,68 @@
 from marshmallow import fields, Schema
-from enum import Enum
 
 
-class UserType(Enum):
-    STUDENT = "student"
-    STAFF = "staff"
+"""
+Admin Model
+
+    id
+    first_name
+    last_name
+    department
+    admin_code
+    email
+    password
+"""
+
+
+"""
+Student Model
+
+    id
+    first_name
+    last_name
+    email
+    faculty
+    department
+    matric_code
+    gpa
+    password
+    registered_courses : relationship
+"""
+
+"""
+Course fields
+
+    id
+    name
+    course_code
+    semester
+    year
+    unit
+    created_at
+    teacher
+    department : foreignKey
+
+"""
+
+"""
+Registered Course fields
+
+    id
+    grade
+    name
+    matric_code
+    unit
+    student_id
+    course_id
+"""
 
 
 class plainCourseRegisteredSchema(Schema):
     id = fields.Int(dump_only=True)
+    grade = fields.Float(dump_only=True)
+    name = fields.Str()
+    matric_code = fields.Str()
+    unit = fields.Int()
     student_id = fields.Int(required=True)
     course_id = fields.Int(required=True)
 
@@ -17,9 +71,9 @@ class plainStudentSchema(Schema):
     id = fields.Int(dump_only=True)
     first_name = fields.Str(required=True)
     last_name = fields.Str(required=True)
-    username = fields.Str(required=True)
+    matric_code = fields.Str(required=True, dump_only=True)
     email = fields.Str(required=True)
-    grade = fields.Int(required=True)
+    department = fields.Str(required=True)
     gpa = fields.Float(required=True)
     password = fields.Str(required=True, load_only=True)
     registered_courses = fields.Nested(plainCourseRegisteredSchema(), many=True)
@@ -37,20 +91,25 @@ class plainCourseSchema(Schema):
     tutor_id = fields.Int(required=True)
 
 
-class plainStaffSchema(Schema):
+class plainAdminSchema(Schema):
     id = fields.Int(dump_only=True)
     first_name = fields.Str(required=True)
     last_name = fields.Str(required=True)
-    username = fields.Str(required=True)
+    department = fields.Str(required=True)
+    admin_code = fields.Str()
     email = fields.Str(required=True)
-    role = fields.Str(required=True)
-    is_admin = fields.Bool(required=True)
     password = fields.Str(required=True, load_only=True)
-    courses = fields.Nested(plainCourseSchema(), many=True)
 
 
 class plainUserLoginSchema(Schema):
-    username = fields.Str(required=True)
-    user_type = fields.Str(required=True)
+    code = fields.Str(required=True)
     password = fields.Str(required=True)
-    confirm_password = fields.Str(required=True)
+
+
+class UserRegisterSchema(Schema):
+    id = fields.Int(dump_only=True)
+    first_name = fields.Str(required=True)
+    last_name = fields.Str(required=True)
+    email = fields.Str(required=True)
+    department = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
