eb24b82a8c77562e02ab746e71832c01a6fff648
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-08 18:35:58 +0100

    utils file

---
+++ api/utils/__init__.py
@@ -0,0 +1,9 @@
+available_departments = {
+    "mech": "Mechanical Engineering",
+    "civ": "Civil Engineering",
+    "csc": "Computer Science",
+    "bch": "Biochemistry",
+    "bio": "Biology",
+    "chem": "Chemistry",
+    "phy": "Physics",
+}
