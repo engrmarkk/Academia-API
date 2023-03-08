037ea38d905f2ff3bd2db6a93538d37a947ba27d
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-08 17:12:46 +0100

    course registered model

--- api/models/course_registered.py
+++ api/models/course_registered.py
@@ -1,12 +1,18 @@
+from sqlalchemy import CheckConstraint
 from ..extensions import db
 
 
 class CourseRegistered(db.Model):
     __tablename__ = 'course_registered'
     id = db.Column(db.Integer, primary_key=True)
-    grade = db.Column(db.Float, default=0.00, nullable=False)
+    grade = db.Column(db.Integer,
+                      CheckConstraint('grade >= 0 AND grade <= 100',
+                                      name='check_grade', deferrable=True,
+                                      initially="DEFERRED",
+                                      info={'min': 'Grade cannot be less than 0', 'max': 'Grade cannot be greater than 100'}))
+    point = db.Column(db.Integer, default=0, nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    user_code = db.Column(db.String(80), nullable=False)
+    matric_code = db.Column(db.String(80), nullable=False)
     unit = db.Column(db.Integer, nullable=False)
     student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
     course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
