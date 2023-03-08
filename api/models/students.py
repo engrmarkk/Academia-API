ef966972f90dcc96c48dc7bbf0e6b9f6d53d78fb
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-08 18:37:02 +0100

    student model

--- api/models/students.py
+++ api/models/students.py
@@ -1,10 +1,19 @@
 from ..extensions import db
 from passlib.hash import pbkdf2_sha256
+import random
+from datetime import datetime
 
 
 def student_default_password(default_password):
     pass_word = pbkdf2_sha256.hash(default_password)
     return pass_word
+
+
+def matric_code_generator(prefix: str):
+    unique_code = random.randint(100000, 999999)
+    unique_code = str(unique_code)
+    mat_num = prefix + unique_code
+    return mat_num
 
 
 class Student(db.Model):
@@ -13,10 +22,10 @@ class Student(db.Model):
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    faculty = db.Column(db.String(70), nullable=False)
     department = db.Column(db.String(70), nullable=False)
-    matric_code = db.Column(db.String(50), unique=True, nullable=False)
-    gpa = db.Column(db.Integer, nullable=False, default=0)
+    matric_code = db.Column(db.String(50), unique=True, nullable=False,
+                            default=matric_code_generator(f'ACA-{datetime.now().year}-'))
+    gpa = db.Column(db.Float, nullable=False, default=0.00)
     password = db.Column(db.Text, nullable=False,
                          default=student_default_password('academia'))
     registered_courses = db.relationship('CourseRegistered',
