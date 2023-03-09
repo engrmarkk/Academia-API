474edd016644b07b62e3919188218b7224a2b8e4
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-09 16:40:50 +0100

    course model modified

--- api/models/courses.py
+++ api/models/courses.py
@@ -5,11 +5,11 @@ from datetime import datetime
 class Course(db.Model):
     __tablename__ = 'courses'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    course_title = db.Column(db.String(100), nullable=False)
     course_code = db.Column(db.String(80), unique=True, nullable=False)
     semester = db.Column(db.Integer, nullable=False)
     year = db.Column(db.Integer, nullable=False, default=datetime.now().year)
-    unit = db.Column(db.Integer, nullable=False)
+    course_unit = db.Column(db.Integer, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
     teacher = db.Column(db.String(150), nullable=False)
     department = db.Column(db.String(150), db.ForeignKey('admin.department'), nullable=False)
