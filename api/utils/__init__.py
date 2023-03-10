050113fbf36aba8baaa849289a85f16e322dbef3
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-10 17:55:53 +0100

    calculate gpa function using the standard scale of 4.0

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,9 +1,22 @@
-available_departments = {
-    "mech": "Mechanical Engineering",
-    "civ": "Civil Engineering",
-    "csc": "Computer Science",
-    "bch": "Biochemistry",
-    "bio": "Biology",
-    "chem": "Chemistry",
-    "phy": "Physics",
-}
+def calculate_gpa(grades, units):
+    total_grade_points = 0
+    total_credits = 0
+
+    for i in range(len(grades)):
+        if grades[i] >= 80:
+            grade_points = 4.0
+        elif grades[i] >= 65:
+            grade_points = 3.0
+        elif grades[i] >= 55:
+            grade_points = 2.0
+        elif grades[i] >= 40:
+            grade_points = 1.0
+        else:
+            grade_points = 0.0
+
+        total_grade_points += grade_points * units[i]
+        total_credits += units[i]
+
+    gpa = round(total_grade_points / total_credits, 2)
+
+    return gpa
