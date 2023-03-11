6b79e1d6e2383ba045b00f4c1b1159ead3198508
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-11 23:00:10 +0100

    configuration

--- api/config/__init__.py
+++ api/config/__init__.py
@@ -3,19 +3,29 @@ from decouple import config
 from datetime import timedelta
 
 # This is the base directory for the application
-BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 # This is the configuration object that will be used by the application
 class Config:
     # This is the secret key for the application
-    SECRET_KEY = config("SECRET_KEY", "secret")
+    SECRET_KEY = config("SECRET_KEY", "secretkey")
     # This is the expiration minutes for the jwt access token
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
     # This is the expiration days for the jwt refresh token
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     # This is the secret key for the jwt
     JWT_SECRET_KEY = config("JWT_SECRET_KEY", "secret")
+    API_TITLE = 'Student Management System'
+    API_VERSION = 'v1'
+    OPENAPI_VERSION = '3.0.2'
+    OPENAPI_URL_PREFIX = '/'
+    OPENAPI_JSON_PATH = 'openapi.json'
+    OPENAPI_REDOC_PATH = '/redoc'
+    OPENAPI_SWAGGER_UI_PATH = '/swagger-ui'
+    OPENAPI_SWAGGER_UI_URL = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
+    OPENAPI_SWAGGER_UI_VERSION = '3.23.11'
+    OPENAPI_SWAGGER_UI_JSONEDITOR = True
     PROPAGATE_EXCEPTIONS = True
 
 
@@ -39,3 +49,8 @@ class TestConfig(Config):
 # This is the dictionary that contains the configuration object
 config_object = {"appcon": AppConfig,
                  "testcon": TestConfig}
+
+
+"""
+
+"""
