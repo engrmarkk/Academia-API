67053ed043ca51d925ac3513f32e52e894089f91
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-13 00:34:21 +0100

    auth schema

---
+++ api/schemas/auth.py
@@ -0,0 +1,14 @@
+from marshmallow import Schema, fields
+
+
+class plainUserLoginSchema(Schema):
+    code = fields.Str(required=True)
+    password = fields.Str(required=True)
+
+
+class UserRegisterSchema(Schema):
+    id = fields.Int(dump_only=True)
+    first_name = fields.Str(required=True)
+    last_name = fields.Str(required=True)
+    email = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
