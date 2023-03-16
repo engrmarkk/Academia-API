c66c8238a075b8b27730e102b27c83ee6319f2b3
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-16 04:17:40 +0100

    test for available courses

--- api/test/test_course.py
+++ api/test/test_course.py
@@ -13,6 +13,14 @@ class StudentTestCase(unittest.TestCase):
         self.appctx.push()
         self.client = self.app.test_client()
         db.create_all()
+        course = Course(
+                course_code="PYT301",
+                course_title="Advanced Python",
+                course_unit=3,
+                teacher="gideon"
+        )
+        db.session.add(course)
+        db.session.commit()
 
     def tearDown(self):
         db.drop_all()
@@ -42,7 +50,15 @@ class StudentTestCase(unittest.TestCase):
 
         # check if student was created
         courses = Course.query.all()
-        self.assertEqual(len(courses), 1)
-        self.assertEqual(courses[0].id, 1)
-        self.assertEqual(courses[0].course_unit, 2)
-        self.assertEqual(courses[0].course_code, "CSC401")
+        self.assertEqual(len(courses), 2)
+        self.assertEqual(courses[1].id, 2)
+        self.assertEqual(courses[1].course_unit, 2)
+        self.assertEqual(courses[1].course_code, "CSC401")
+
+    def test_available_course(self):
+        course = Course.query.all()
+        self.assertEqual(len(course), 1)
+        self.assertEqual(course[0].id, 1)
+        self.assertEqual(course[0].course_code, "PYT301")
+        self.assertEqual(course[0].course_unit, 3)
+        self.assertEqual(course[0].teacher, "gideon")
