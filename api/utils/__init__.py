056fa0d3ee718963753c053221e117388e615b46
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-17 14:31:45 +0100

    get grade function

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,15 +1,15 @@
-def calculate_gpa(grades, units):
+def calculate_gpa(scores, units):
     total_grade_points = 0
     total_credits = 0
 
-    for i in range(len(grades)):
-        if grades[i] >= 80:
+    for i in range(len(scores)):
+        if scores[i] >= 80:
             grade_points = 4.0
-        elif grades[i] >= 65:
+        elif scores[i] >= 65:
             grade_points = 3.0
-        elif grades[i] >= 55:
+        elif scores[i] >= 55:
             grade_points = 2.0
-        elif grades[i] >= 40:
+        elif scores[i] >= 40:
             grade_points = 1.0
         else:
             grade_points = 0.0
@@ -20,3 +20,16 @@ def calculate_gpa(grades, units):
     gpa = round(total_grade_points / total_credits, 2)
 
     return gpa
+
+
+def get_grade(score):
+    if score >= 80:
+        return 'A'
+    elif score >= 65:
+        return 'B'
+    elif score >= 55:
+        return 'C'
+    elif score >= 40:
+        return 'D'
+    else:
+        return 'F'
