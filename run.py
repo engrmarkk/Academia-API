f381fdafaa723b1f23bfcae89f006e5bfa876ff3
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-17 16:35:38 +0100

    create app for production

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from api import create_app
+from api.config import config_object
 
-app = create_app()
+
+app = create_app(configure=config_object['prodcon'])
 
 if __name__ == "__main__":
     app.run(debug=True)
